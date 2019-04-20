46930fb3a5873c9e380db3c7cb0f086803eb7231
Jared Hubbard <jahubbard@csumb.edu>
2019-04-20 20:56:20 +0000

    Added projector metrics to dashboard and total devices deployed metric

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190420174540) do
+ActiveRecord::Schema.define(version: 20190420200126) do
 
   create_table "cameras", force: :cascade do |t|
     t.string "manufacture"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20190420174540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "total_lamp_hours"
+    t.date "model_year"
   end
 
   create_table "switchers", force: :cascade do |t|
